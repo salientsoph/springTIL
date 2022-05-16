6ab3c4f102296dcd4508cd746478f3d8524fe6e1
salientsoph <salientsoph@gmail.com>
2022-05-17 00:00:28 +0900

    controller

--- hello_spring/src/main/java/hello/hello_spring/controller/HelloController.java
+++ hello_spring/src/main/java/hello/hello_spring/controller/HelloController.java
@@ -3,13 +3,51 @@ package hello.hello_spring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
 
+    //static 방식
     @GetMapping("hello") //web application에서 'hello' 라고 들어오면 이 메소드 호출
     public String hello(Model model){
         model.addAttribute("data","hello!!");
         return "hello";
+    }
+
+    //mvc방식
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name")String name, Model model){
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
+    //api방식(1) 그냥 문자열만 전달하는 경우
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name){
+        return "hello " + name;
+    }
+
+    //api방식(2) 데이터를 전달하는 경우
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name){
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello{
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
